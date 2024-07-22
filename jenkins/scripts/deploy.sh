d3b660d63ee4cbe7899f90286ebf72faef694cc1
Ernest Foo <113896138+ErnestFoo@users.noreply.github.com>
2024-07-22 21:26:16 +0800

    chore: Update docker volume path in deploy script

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v //C:/Users/Fooyo/Desktop/School/Year\ 2\ Tri\ 3/SSD/Labs/Lab\ 7/jenkins-php-selenium-/jenkins-php-selenium-test/src://var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v //C/Users/Fooyo/Desktop/School/Year\ 2\ Tri\ 3/SSD/Labs/Lab\ 7/jenkins-php-selenium-/jenkins-php-selenium-test/src://var/www/html php:7.2-apache
 sleep 1
 set +x
 
